ddf806cd67216c17bed770ec572613a1a4d34af1
donaldr <DonaldR@nvisionit.co.za>
2015-10-30 13:21:23 +0200

    Added projects to make available in Azure

---
+++ SimpleJenkinsTest/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SimpleJenkinsTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        [ExpectedException(typeof(NotImplementedException))]
+        public void TestMethod1()
+        {
+
+            throw new NotImplementedException();
+
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
